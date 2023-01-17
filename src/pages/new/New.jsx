f72590f693446482cdec1a1061f1071f318f4fc3
Sanusi Olaide <kalloello@gmail.com>
2023-01-17 16:09:04 +0100

    working on the first part of the app using router

---
+++ src/pages/new/New.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./new.scss";
+
+function New () {
+    return (
+        <div className="new">
+            New
+        </div>
+    )
+}
+
+export default New
---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./single.scss";
+
+function Single () {
+    return (
+        <div className="single">
+            Single
+        </div>
+    )
+}
+
+export default Single
